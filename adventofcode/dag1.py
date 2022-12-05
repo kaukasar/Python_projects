38bb1cdd5e98648aa15e78585ed86089d5ddd3c5
Vladislav Beliaev <vladislav.beliaev@arbetsformedlingen.se>
2022-12-05 17:15:15 +0100

    mer kalender

---
+++ adventofcode/dag1.py
@@ -0,0 +1,22 @@
+with open('dag1.txt', 'r') as f:
+    innehåll = f.readlines()
+    summa = 0
+    räknare = 0
+    dicten = {}
+    totalLista = []
+    for i in innehåll:
+        if i != '\n':
+            summa += int(i)
+        else:
+            dicten[räknare] = summa
+            summa = 0
+            räknare += 1
+    summa = 0
+    for nyckel in dicten:
+        totalLista.append(dicten.get(nyckel))
+        if dicten.get(nyckel) > summa:
+            summa = dicten[nyckel]
+            elf = nyckel
+    totalLista.sort()
+    print(f'Elf {elf} har flest kalorier, {summa} st.')
+    print(f'De 3 Elf som har mest kalorier har tillsammans {totalLista[-1] + totalLista[-2] + totalLista[-3]} st.')
--- adventofcode/dag4.py
+++ adventofcode/dag4.py
@@ -1,23 +1,19 @@
 with open('input4.txt', 'r') as f:
     #rad = f.readline().strip()
     counter = 0
-    totalsträng = ''
     lista = []
-    for antalrader, rad in enumerate(f):
+    for rad in f:
         rent = rad.strip()
         s = rent.replace('-', ',').split(',')
-        totalsträng += f'{s[0]},{s[1]},{s[2]},{s[3]}'
         #print(s)
-        a, b, c, d = int(s[0]), int(s[1]) ,int(s[2]), int(s[3])
+        a, b, c, d = int(s[0]), int(s[1]), int(s[2]), int(s[3])
 
-        lista.append((a, b))
-        lista.append((c, d))
-
-        if (a <= c and b >= d) or (c <= a and d >= b):
+        #lista.append((a, b))
+        #lista.append((c, d))
+        #if (a <= c and b >= d) or (c <= a and d >= b): Detta tillhör del 1
+        if b >= c and a <= d:
             counter += 1
+            print(a, b, c, d)
+            #print(f'{b} {c}')
 
-print(lista)
 print(counter)
-for element in lista:
-    print(element[0])
-    print(element[1])
