74093ce121dd2178c8cc749eb7c91029a80a7b0c
kaukasar <kona36@hotmail.com>
2022-12-09 20:32:04 +0100

    lite mer ändringar

--- adventofcode/dag7.py
+++ adventofcode/dag7.py
@@ -9,7 +9,10 @@ class Node:
         return str(self.data)
 
     def get_child(self):
-        return str(self.children)
+        return self.children
+
+    def get_parent(self):
+        pass
 
     def add_child(self, obj):
         self.children.append(obj)
@@ -19,9 +22,13 @@ root = Node("roten")
 a = Node("hej")
 b = Node("japp")
 c = Node(42)
+d = Node(True)
 
-root.add_child(a)
-a.add_child(b)
-root.add_child(c)
+root.add_child(a.data)
+a.add_child(b.data)
+root.add_child(c.data)
+c.add_child(d.data)
 
 print(root.get_child())
+print(c.get_child())
+print(d.get_child())
